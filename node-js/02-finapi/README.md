d7f43164026dc5a363f34eeca7df6f03126c276c
Anderson Suga <anderson.suga@gmail.com>
2022-09-20 11:53:07 -0300

    10 - Remover conta

--- node-js/02-finapi/README.md
+++ node-js/02-finapi/README.md
@@ -11,7 +11,8 @@
 - [x] Deve ser possível buscar o extrato bancário do cliente por data
 - [x] Deve ser possível atualizar dados da conta do cliente
 - [x] Deve ser possível obter dados da conta do client
-- [] Deve ser possível deletar uma conta
+- [x] Deve ser possível deletar uma conta
+- [x] Deve ser possível o balance de uma conta
 
 ---
 
--- node-js/02-finapi/src/index.js
+++ node-js/02-finapi/src/index.js
@@ -1,4 +1,3 @@
-const { request, response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -71,6 +70,15 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  // splice
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
 // app.use(verifyIfExistsAccountCPF);
 
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
@@ -131,6 +139,14 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
 });
 
 app.listen(3333);
