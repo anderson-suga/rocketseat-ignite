dc6c4417587a27c959cb36a063bd0a5acac975f3
Anderson Suga <anderson.suga@gmail.com>
2022-09-20 15:37:19 -0300

    02 - Debugando a aplicação

--- node-js/04-rentalx/src/server.ts
+++ node-js/04-rentalx/src/server.ts
@@ -1,9 +1,17 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.json({mesage: "Hello World!!!"})
-})
+app.use(express.json());
 
-app.listen(3333, () => console.log("Server is running!!!" ));
+app.get("/", (request, response) => {
+  response.json({ mesage: "Hello World!!!" });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
+app.listen(3333, () => console.log("Server is running!!!"));
