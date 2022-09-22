ad2822b08364d26cf157ecb4aeb3c239164b0a8a
Anderson Suga <anderson.suga@gmail.com>
2022-09-22 10:41:37 -0300

    Criando use case de especificação

--- node-js/04-rentalx/src/routes/specifications.routes.ts
+++ node-js/04-rentalx/src/routes/specifications.routes.ts
@@ -1,21 +1,10 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationServices";
+import { createSpecificationsController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.execute({ description, name });
-
-  return response.status(201).send();
+  return createSpecificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
