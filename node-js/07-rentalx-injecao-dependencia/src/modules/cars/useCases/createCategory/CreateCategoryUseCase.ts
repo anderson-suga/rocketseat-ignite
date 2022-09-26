28e466090b0a53ec3205e6c5f965da6aed6abad4
Anderson Suga <anderson.suga@gmail.com>
2022-09-26 15:59:53 -0300

    Teste de criação de categoria

--- node-js/07-rentalx-injecao-dependencia/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ node-js/07-rentalx-injecao-dependencia/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/AppError";
 
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
@@ -21,7 +22,7 @@ class CreateCategoryUseCase {
     );
 
     if (categoryAlredyExists) {
-      throw new Error("Category already exists");
+      throw new AppError("Category already exists");
     }
 
     this.categoriesRepository.create({ name, description });
