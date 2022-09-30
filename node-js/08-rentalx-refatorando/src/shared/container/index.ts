8e8e996c60ec41c817bf9082ae9341d6e8ff1da5
Anderson Suga <anderson.suga@gmail.com>
2022-09-30 11:49:11 -0300

    Continuação da listagem de carros disponíveis

--- node-js/08-rentalx-refatorando/src/shared/container/index.ts
+++ node-js/08-rentalx-refatorando/src/shared/container/index.ts
@@ -10,8 +10,8 @@ import { SpecificationsRepository } from "../../modules/cars/infra/typeorm/repos
 import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { UsersRepository } from "../../modules/accounts/infra/typeorm/repositories/UsersRepository";
 
-import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
-import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
+import { ICarsRepository } from "../../modules/cars/repositories/ICarsRepository";
+import { CarsRepository } from "../../modules/cars/infra/typeorm/repositories/CarsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
