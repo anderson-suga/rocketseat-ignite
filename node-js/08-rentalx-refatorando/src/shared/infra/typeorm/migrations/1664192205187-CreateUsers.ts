419973d7a893f472cec8208f0529765428a1df7d
Anderson Suga <anderson.suga@gmail.com>
2022-10-03 08:53:14 -0300

    Criando migrations do aluguel

--- node-js/08-rentalx-refatorando/src/shared/infra/typeorm/migrations/1664192205187-CreateUsers.ts
+++ node-js/08-rentalx-refatorando/src/shared/infra/typeorm/migrations/1664192205187-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1664192205187 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
